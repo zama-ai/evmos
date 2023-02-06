2fdfec832861f8fd100377f9418c0ae7ec143ce7
tremblaythibaultl <tremblaythibaultl@gmail.com>
2023-02-06 22:03:18 +0100

    Fix init validator script

--- init_validator.sh
+++ init_validator.sh
@@ -8,6 +8,6 @@ COSMOS_BUILD_OPTIONS=nostrip make install
 ./setup.sh
 
 touch $HOME/privkey
-$EVMOSD keys unsafe-export-eth-key mykey1 --keyring-backend test > $HOME/privkey
+evmos keys unsafe-export-eth-key mykey1 --keyring-backend test > $HOME/privkey
 touch $HOME/node_id
-$EVMOSD tendermint show-node-id > $HOME/node_id
+evmos tendermint show-node-id > $HOME/node_id
