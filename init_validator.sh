166a10bc71cc421672ad92cc2f8522430c616ea4
tremblaythibaultl <tremblaythibaultl@gmail.com>
2023-02-06 22:22:33 +0100

    Fix typo in script

--- init_validator.sh
+++ init_validator.sh
@@ -8,6 +8,6 @@ COSMOS_BUILD_OPTIONS=nostrip make install
 ./setup.sh
 
 touch $HOME/privkey
-evmos keys unsafe-export-eth-key mykey1 --keyring-backend test > $HOME/privkey
+evmdds keys unsafe-export-eth-key mykey1 --keyring-backend test > $HOME/privkey
 touch $HOME/node_id
-evmos tendermint show-node-id > $HOME/node_id
+evmosd tendermint show-node-id > $HOME/node_id
