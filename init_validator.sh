969360371d521335382604fc6d36554c3b848a57
tremblaythibaultl <tremblaythibaultl@gmail.com>
2023-02-08 09:51:12 +0100

    Remove wipe of evmosd config folder

--- init_validator.sh
+++ init_validator.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
-# Clear everything of previous installation
-rm -rf ~/.evmosd*
-
-# Reinstall daemon
 COSMOS_BUILD_OPTIONS=nostrip make install
 
 ./setup.sh
-
-cp /keys/network-fhe-public-keys/* ~/.evmosd/zama/keys/network-fhe-keys
-cp /keys/network-fhe-private-keys/* ~/.evmosd/zama/keys/network-fhe-keys
-cp -r /keys/users-fhe-keys ~/.evmosd/zama/keys
 
 touch $HOME/privkey
 evmosd keys unsafe-export-eth-key mykey1 --keyring-backend test > $HOME/privkey
