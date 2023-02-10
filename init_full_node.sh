083b6cff1d2034024c0de7ca0c0a77bba51a9f42
tremblaythibaultl <tremblaythibaultl@gmail.com>
2023-02-10 09:15:48 +0000

    remove genesis manipulations

--- init_full_node.sh
+++ init_full_node.sh
@@ -9,10 +9,3 @@ COSMOS_BUILD_OPTIONS=nostrip make install
 
 cp /keys/network-fhe-public-keys/* ~/.evmosd/zama/keys/network-fhe-keys
 cp -r /keys/users-fhe-keys ~/.evmosd/zama/keys
-
-#get genesis.json and privkey from AWS
-cp ~/genesis.json ~/.evmosd/config/
-NODE_ID=`cat ~/node_id` 
-cp ~/.evmosd/config/config.toml temp
-awk -v node_id=$NODE_ID 'NR==212 { sub("\"\"", "\""node_id"@15.188.64.234:26656\"") }; { print }' temp >~/.evmosd/config/config.toml
-rm temp
