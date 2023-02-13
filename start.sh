684dbef09cb7dbecb7b508b86bf8240df14b3c17
Louis Tremblay Thibault <tremblaythibaultl@gmail.com>
2023-02-13 13:17:45 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,6 +4,9 @@ LOGLEVEL="info"
 # Recompile, if needed.
 COSMOS_BUILD_OPTIONS=nostrip make install
 
+cp /node_config/genesis.json ~/.evmosd/config
+cp /node_config/config.toml ~/.evmosd/config
+
 # Start the node (remove the --pruning=nothing flag if historical queries are not needed)
 evmosd start --pruning=nothing $TRACE --log_level $LOGLEVEL \
         --minimum-gas-prices=0.000000000000000001aevmos \
