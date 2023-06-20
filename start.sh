be4e0923cfe3916d93e183872f959fd261a53a94
Petar Ivanov <29689712+dartdart26@users.noreply.github.com>
2023-06-20 11:34:52 +0300

    Change config for public devnet (#143)
    Since we now have gas costs for FHE operations, set block gas limit to
    100000000 gas. As a reference, an ERC20 transfer estimates to around
    1220732 gas for euint32, meaning a block could have up to 100 ERC20
    transfers. These values might change as we test more, but we need a
    starting point.
    
    Since we now have compact ciphertexts for input, set max block size to
    4194304 (4MB).
    
    Disable the `personal` and `debug` namespaces on the JSON-RPC server.
    
    Reinstate the minimum gas price to 0.0001aevmos (as it was before our
    FHE modifications).
    
    Put a gas cap of 50000000 gas for `eth_call` (i.e. view functions) over
    the JSON-RPC interface. That limits the amount of computation users can
    do "for free" over JSON-RPC. 50000000 is 1/2 max block gas.
    
    Remove custom gas prices and adjustments that we added initially when
    we worked with bigger ciphertexts and proofs.

--- start.sh
+++ start.sh
@@ -6,8 +6,6 @@ COSMOS_BUILD_OPTIONS=nostrip make install
 
 # Start the node (remove the --pruning=nothing flag if historical queries are not needed)
 evmosd start --pruning=nothing $TRACE --log_level $LOGLEVEL \
-        --minimum-gas-prices=0.000000000000000001aevmos \
-        --json-rpc.gas-cap=9999999999999999 \
-        --gas-prices=0.00000000000000000000000000000000001aev0mos \
-        --gas-adjustment=0.000000000000000000000000001 \
-        --json-rpc.api eth,txpool,personal,net,debug,web3
+        --minimum-gas-prices=0.0001aevmos \
+        --json-rpc.gas-cap=50000000 \
+        --json-rpc.api eth,txpool,net,web3
