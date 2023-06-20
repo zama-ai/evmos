5c0bc1c306b62d53436616bf9389e9d7b712c4ff
Petar Ivanov <29689712+dartdart26@users.noreply.github.com>
2023-06-20 11:32:48 +0300

    Lower JSON-RPC gas cap to 1/2 max block gas

--- start.sh
+++ start.sh
@@ -7,5 +7,5 @@ COSMOS_BUILD_OPTIONS=nostrip make install
 # Start the node (remove the --pruning=nothing flag if historical queries are not needed)
 evmosd start --pruning=nothing $TRACE --log_level $LOGLEVEL \
         --minimum-gas-prices=0.0001aevmos \
-        --json-rpc.gas-cap=500000000 \
+        --json-rpc.gas-cap=50000000 \
         --json-rpc.api eth,txpool,net,web3
